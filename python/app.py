a6cea238886711f4ffeb1016040cbe5a500716fa
cloudwwh <56953724+cloudwwh@users.noreply.github.com>
2020-04-15 18:52:45 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Docker Image Pull Action"
+    return "Hello World! Docker Image Pull Action Success"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
